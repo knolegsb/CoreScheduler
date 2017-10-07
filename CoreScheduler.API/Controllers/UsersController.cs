34840a05152f02503218d0194bd7ebb46b91e49c
Sean <seon2you@hotmail.com>
2017-10-06 22:00:35 -0700

    user api done

--- CoreScheduler.API/Controllers/UsersController.cs
+++ CoreScheduler.API/Controllers/UsersController.cs
@@ -59,5 +59,113 @@ namespace CoreScheduler.API.Controllers
             Response.AddPagination(page, pageSize, totalUsers, totalPages);
             return new OkObjectResult(_usersVM);
         }
+
+        [HttpGet("{id}", Name = "GetUser")]
+        public IActionResult Get(int id)
+        {
+            User _user = _userRepository.GetSingle(u => u.Id == id, u => u.SchedulesCreated);
+
+            if (_user != null)
+            {
+                UserViewModel _userVM = Mapper.Map<User, UserViewModel>(_user);
+                return new OkObjectResult(_userVM);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("{id}/schedules", Name = "GetUserSchedules")]
+        public IActionResult GetSchedules(int id)
+        {
+            IEnumerable<Schedule> _userSchedules = _scheduleRepository.FindBy(s => s.CreatorId == id);
+
+            if (_userSchedules != null)
+            {
+                IEnumerable<ScheduleViewModel> _userSchedulesVM = Mapper.Map<IEnumerable<Schedule>, IEnumerable<ScheduleViewModel>>(_userSchedules);
+                return new OkObjectResult(_userSchedulesVM);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]UserViewModel user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            User _newUser = new User { Name = user.Name, Profession = user.Profession, Avatar = user.Avatar };
+
+            _userRepository.Add(_newUser);
+            _userRepository.Commit();
+
+            user = Mapper.Map<User, UserViewModel>(_newUser);
+
+            CreatedAtRouteResult result = CreatedAtRoute("GetUser", new { controller = "Users", id = user.Id }, user);
+            return result;
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]UserViewModel user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            User _userDb = _userRepository.GetSingle(id);
+
+            if (_userDb == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _userDb.Name = user.Name;
+                _userDb.Profession = user.Profession;
+                _userDb.Avatar = user.Avatar;
+                _userRepository.Commit();
+            }
+
+            user = Mapper.Map<User, UserViewModel>(_userDb);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            User _userDb = _userRepository.GetSingle(id);
+
+            if (_userDb == null)
+            {
+                return new NotFoundResult();
+            }
+            else
+            {
+                IEnumerable<Attendee> _attendees = _attendeeRepository.FindBy(a => a.UserId == id);
+                IEnumerable<Schedule> _schedules = _scheduleRepository.FindBy(s => s.CreatorId == id);
+
+                foreach (var attendee in _attendees)
+                {
+                    _attendeeRepository.Delete(attendee);
+                }
+
+                foreach (var schedule in _schedules)
+                {
+                    _attendeeRepository.DeleteWhere(a => a.ScheduleId == schedule.Id);
+                    _scheduleRepository.Delete(schedule);
+                }
+
+                _userRepository.Delete(_userDb);
+                _userRepository.Commit();
+                return new NoContentResult();
+            }
+        }
     }
 }
